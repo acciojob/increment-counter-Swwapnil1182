d00aeec80039de48b3b416deba0d440ddd2fdd31
Swwapnil1182 <swapniljadhav1182@gmail.com>
2025-05-08 19:45:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+let sample=document.getElementById("counter")
+count=0
+let  incBtn=document.getElementById("incrementBtn")
+
+function functionOnClick(){
+	alert(count)
+	count++
+	sample.innerHTML=count
+}
